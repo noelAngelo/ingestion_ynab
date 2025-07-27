c13f5f64bdac872460d24be13e3b870ef090f6df
Noel Borneo <born.angelonoel@gmail.com>
2025-07-27 22:33:38 +1000

    updates

--- sources/api/upbank/source.py
+++ sources/api/upbank/source.py
@@ -16,8 +16,26 @@ def upbank(upbank_token: str = dlt.secrets.value):
                 "next_url_path": "links.next",
             },
         },
+        "resource_defaults": {"primary_key": "id", "write_disposition": "merge"},
         "resources": [
-            "transactions",
+            {
+                "name": "transactions",
+                "endpoint": {
+                    "path": "/transactions",
+                    "method": "GET",
+                    "params": {
+                        "sort": "createdAt",
+                        "direction": "desc",
+                        "since": {
+                            "type": "incremental",
+                            "cursor_path": "attributes.createdAt",
+                            "initial_value": "2017-01-01T00:00:00Z",
+                        },
+                    },
+                    "data_selector": "data",  # selects the list of records
+                },
+            }
         ],
     }
+
     yield from rest_api_resources(config)
